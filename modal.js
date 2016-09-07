dd47674d68cbd638b53b6c1bb5e6afc5b49282ad
savioabuga <savioabuga@gmail.com>
2016-09-07 22:13:53 +0300

    defining the modal iife

--- modal.js
+++ modal.js
@@ -1,6 +1,35 @@
 (function(){
     // Define our constructor
     this.Modal = function() {
+        this.closeButton = null;
+        this.modal = null;
+        this.overlay = null;
 
+        var defaults = {
+            className: 'fade-and-drop',
+            closeButton: true,
+            content: "",
+            maxWidth: 600,
+            minWidth: 200,
+            overlay: true
+        };
+
+        // Create options by extending defaults with the passed arguments
+        if(arguments[0] && typeof arguments[0] === "object") {
+            this.options = extendDefaults(defaults, arguments[0])
+        }
+
+        function extendDefaults(source, properties) {
+            var property;
+            for(property in properties) {
+                if (properties.hasOwnProperty(property)) {
+                    source[property] = properties[property];
+                }
+            }
+            return source;
+        }
     };
+
 })();
+
+
